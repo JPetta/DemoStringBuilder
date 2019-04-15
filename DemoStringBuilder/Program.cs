d13b481f7f95b7eb677cb892ca2cc0fa0d7ed2c7
JPetta <jovipetraratulangi@gmail.com>
2019-04-15 14:47:22 +0700

    first commit

---
+++ DemoStringBuilder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace DemoStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.Append('-', 10)
+            .AppendLine()
+            .Append("header")
+            .AppendLine()
+            .Append('-', 10)
+            .AppendLine()
+            .Replace('-', '+')
+            .Remove(0, 10)
+            .Insert(0, new String('-', 10));
+
+            Console.WriteLine(builder);
+        }
+    }
+}
